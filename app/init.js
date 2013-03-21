74442f30f56061ee5da6e9cefbbf3283da408a88
Justin Winslow <jwin.me>
2013-03-20 22:11:58 -0500

    Style tweaks and initial FB integration

--- app/init.js
+++ app/init.js
@@ -9,48 +9,54 @@ if (debug) console.log('init');
 
 // Require config
 require.config({
-    paths: {
-        // Libraries
-        jquery: 'components/jquery/jquery',
-        //underscore: 'components/underscore', I left this as a comment so you can see how aliasing paths can make "upgrades" easier
-        underscore: 'components/lodash/lodash',
-        backbone: 'components/backbone/backbone',
-        handlebars: 'components/handlebars/handlebars',
-        modernizr: 'components/modernizr/modernizr',
-        text: 'components/text/text',
-        css: 'components/css/css',
+  paths: {
+    // Libraries
+    jquery: 'components/jquery/jquery',
+    //underscore: 'components/underscore', I left this as a comment so you can see how aliasing paths can make "upgrades" easier
+    underscore: 'components/lodash/lodash',
+    backbone: 'components/backbone/backbone',
+    handlebars: 'components/handlebars/handlebars',
+    modernizr: 'components/modernizr/modernizr',
+    text: 'components/text/text',
+    css: 'components/css/css'
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
     },
-    shim: {
-        backbone: {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        underscore: {
-            exports: '_'
-        },
-        handlebars: {
-            exports: 'Handlebars'
-        },
-        modernizr: {
-            exports: 'Modernizr'
-        }
+    underscore: {
+      exports: '_'
+    },
+    handlebars: {
+      exports: 'Handlebars'
+    },
+    modernizr: {
+      exports: 'Modernizr'
     }
+  }
 });
 
 // Start app
 require(
-    [
-        'jquery',
-        'underscore',
-        'backbone',
-        'handlebars'
-    ],
-    function ($, _, Backbone, Handlebars, somemodule) {
-        if (debug) console.log($);
-        if (debug) console.log(_);
-        if (debug) console.log(Backbone);
-        if (debug) console.log(Handlebars);
+  [
+    'jquery',
+    'underscore',
+    'backbone',
+    'handlebars',
+    'modules/fbUser'
+  ],
+  function ($, _, Backbone, Handlebars, fbUser) {
+    if (debug) console.log($);
+    if (debug) console.log(_);
+    if (debug) console.log(Backbone);
+    if (debug) console.log(Handlebars);
+    if (debug) console.log(fbUser);
 
-        window.myApp = window.myApp = {};
-    }
+    window.myApp = window.myApp = {};
+
+    myApp.modules = {};
+
+    myApp.modules.jade = new fbUser.model();
+  }
 );
